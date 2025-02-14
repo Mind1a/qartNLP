698fe39a84dc694c606d1288c8fd424444ab2be9
Tamar Robakidze <troba12@freeuni.edu.ge>
2025-02-15 00:53:13 +0400

    form submit functionality
    add functionality for  form submission.

--- js/validation.js
+++ js/validation.js
@@ -1,9 +1,10 @@
 const enterText = document.querySelector(".enterText");
+const formContainer = document.querySelector(".form-container");
 
 const validWordsNumber = (inputs) => inputs.trim().split(" ").length > 250;
 const requiredInput = (input) => input.trim() !== "";
  
-const checkDescriptionValidation = function () {
+const checkTextareaValidation = function () {
   let result = true;
   const enterTextBox = enterText.value;
 
@@ -12,15 +13,40 @@ const checkDescriptionValidation = function () {
     enterText.classList.add("is-valid");
   }
 
-  if (!validWordsNumber(description)) {
+  if (!validWordsNumber(enterTextBox)) {
     enterText.classList.add("not-valid");
     result = false;
   }
-  if (!requiredInput(description)) {
+  if (!requiredInput(enterTextBox)) {
     enterText.classList.add("not-valid");
     result = false;
   }
   return result;
 };
+checkTextareaValidation();
+
+formContainer.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const validTextarea = checkTextareaValidation();
+  if (!validTextarea) return;
+
+
+  const formData = new FormData();
+  formData.append("position", enterText.value);
+
+  fetch(
+    "https://api.jansjansKAMLnisnZ.ahsbhbabJANan.ge/api/ssssjsn",
+    {
+      method: "POST",
+      headers: {
+        accept: "application/json",
+      },
+      body: formData,
+    }
+  )
+    .then((response) => response.json())
+    .then((res) => console.log(res));
+});
 
  
