2dbb43607598c657a0d28fe65018404f70cd84dc
Irakli00 <iraklitsurtsumia@gmail.com>
2025-03-02 15:28:19 +0400

    enterText and a links

--- js/lamatizationValidation.js
+++ js/lamatizationValidation.js
@@ -1,16 +1,37 @@
 const enterText = document.querySelector(".enterText");
+const form = document.querySelector(".form-container");
 const formContainer = document.querySelector(".form-container");
 const AmountWords = document.querySelector(".wordsAmount");
 const wordsAmountBox = document.querySelector(".words-Amount-Box");
+const clearBtn = document.querySelector(".btn-clear");
+const submitBtn = document.querySelector(".btn-analize");
 
 const validWordsNumber = (inputs) => inputs.trim().split("").length;
-
 const requiredInput = (input) => input.trim() !== "";
+
+clearBtn.disabled = true;
+submitBtn.disabled = true;
 
 enterText.addEventListener("input", function (e) {
   e.preventDefault();
+  clearBtn.disabled = !requiredInput(enterText.value);
+  submitBtn.disabled = !requiredInput(enterText.value);
+
   const item = validWordsNumber(enterText.value);
-  console.log(item);
+  const html = `
+     <span class="wordsAmount">${item}/250</span>
+   `;
+  wordsAmountBox.innerHTML = "";
+  wordsAmountBox.insertAdjacentHTML("beforeend", html);
+});
+
+form.addEventListener("reset", function (e) {
+  clearBtn.disabled = true;
+  submitBtn.disabled = true;
+
+  enterText.value = "";
+
+  const item = validWordsNumber(enterText.value);
   const html = `
      <span class="wordsAmount">${item}/250</span>
    `;
